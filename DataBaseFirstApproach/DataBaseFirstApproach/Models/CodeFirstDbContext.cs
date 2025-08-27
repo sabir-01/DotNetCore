06097ace0ad7f9afb731a4cd1b03312bcc6b433e
sabir-01 <muhammd031883@gmail.com>
2025-08-27 11:47:05 +0500

    Initial commit: Add DataBaseFirstApproach project

---
+++ DataBaseFirstApproach/DataBaseFirstApproach/Models/CodeFirstDbContext.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataBaseFirstApproach.Models;
+
+public partial class CodeFirstDbContext : DbContext
+{
+    public CodeFirstDbContext()
+    {
+    }
+
+    public CodeFirstDbContext(DbContextOptions<CodeFirstDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+        }
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.EmployeeId).HasName("PK__employee__C52E0BA8A8FDCCCF");
+
+            entity.ToTable("employees");
+
+            entity.Property(e => e.EmployeeId).HasColumnName("employee_id");
+            entity.Property(e => e.Email)
+                .HasMaxLength(50)
+                .HasColumnName("email");
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(30)
+                .HasColumnName("first_name");
+            entity.Property(e => e.JoiningDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime")
+                .HasColumnName("joining_date");
+            entity.Property(e => e.LastName)
+                .HasMaxLength(30)
+                .HasColumnName("last_name");
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.Property(e => e.StudentGender)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.StudentName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.StudentStandard)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasDefaultValue("");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DataBaseFirstApproach/DataBaseFirstApproach/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataBaseFirstApproach.Models;
+
+public partial class Employee
+{
+    public int EmployeeId { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string? Email { get; set; }
+
+    public DateTime? JoiningDate { get; set; }
+}
